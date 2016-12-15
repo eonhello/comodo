b37ada5c3a87dae69d603d0bc10ae0dec0d88b38
raeoon0511 <raeoon0511@gmail.com>
2016-12-15 17:29:28 +0900

    b

--- src/board/sleep/Action.java
+++ src/board/sleep/Action.java
@@ -3,6 +3,6 @@ package board.sleep;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public interface Action {
+public interface Action {//
 	public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception;
 }
